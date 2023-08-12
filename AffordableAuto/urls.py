73f855a390f50ba6c90f28228760245f76bc97ac
baileyrechkemmer <rechkemmer3@gmail.com>
2023-08-12 10:32:03 -0500

    setting up path 3

--- AffordableAuto/urls.py
+++ AffordableAuto/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-import pages
-import pages.views
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', pages.views.home, name='home'),
+    path('', include('pages.urls')),
 ]
